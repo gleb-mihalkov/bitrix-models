19411458a889ee287386a77a86969fad3afd2f75
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-07-22 01:47:13 +0300

    Генерация документации.

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","BitrixModel\\Database"],["c","BitrixModel\\Element"],["c","BitrixModel\\Highload"],["c","BitrixModel\\Section"],["c","BitrixModel\\User"]];
