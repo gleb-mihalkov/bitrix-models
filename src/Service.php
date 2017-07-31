65c909521ce91440e122eb19c8fdb4e9557196a8
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-08-01 02:13:19 +0300

    Добавление событий Eloquent.

--- src/Service.php
+++ src/Service.php
@@ -2,6 +2,10 @@
 namespace BitrixModel
 {
     use Arrilot\BitrixModels\ServiceProvider;
+    use Bitrix\Main\Config\Configuration;
+    use Illuminate\Database\Capsule\Manager as Capsule;
+    use Illuminate\Container\Container;
+    use Illuminate\Events\Dispatcher;
 
     /**
      * Сервис моделей.
@@ -15,7 +19,45 @@ namespace BitrixModel
         public static function init()
         {
             ServiceProvider::register();
-            ServiceProvider::registerEloquent();
+            self::initEloquent();
+        }
+
+        /**
+         * Инициализирует модели Eloquent.
+         * @return void
+         */
+        protected static function initEloquent()
+        {
+            $config = Configuration::getInstance();
+            $config = $config->get('connections');
+            $config = $config['default'];
+
+            $container = Container::getInstance();
+
+            if (!$container) {
+                $container = new Container();
+                Container::setInstance($container);
+            }
+
+            $capsule = new Capsule($container);
+
+            $capsule->addConnection([
+                'driver'    => 'mysql',
+                'host'      => $config['host'],
+                'database'  => $config['database'],
+                'username'  => $config['login'],
+                'password'  => $config['password'],
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => '',
+                'strict'    => false,
+            ]);
+
+            $dispatcher = new Dispatcher($container);
+            $capsule->setEventDispatcher($dispatcher);
+
+            $capsule->setAsGlobal();
+            $capsule->bootEloquent();
         }
     }
 }
