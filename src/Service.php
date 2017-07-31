d8d834f44c02b47541eb96f9c4c77af3910bcba3
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-08-01 02:26:51 +0300

    Убираем лишний метод из конфигурации.

--- src/Service.php
+++ src/Service.php
@@ -24,6 +24,7 @@ namespace BitrixModel
 
         /**
          * Инициализирует модели Eloquent.
+         * @internal
          * @return void
          */
         protected static function initEloquent()
